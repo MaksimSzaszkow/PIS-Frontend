65800eff7eb82680a83ca3eeb50e111d8d49decd
MaksimSzaszkow <35842893+MaksimSzaszkow@users.noreply.github.com>
2022-12-30 01:12:23 +0100

    Tests scaffolding

---
+++ src/hooks/AuthContext.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+describe("Login", () => {
+  test("Displays user info on success", () => {});
+
+  test("Displays error on failure", () => {});
+});
+
+describe("Logout", () => {
+  test("Clears user data", () => {});
+});
+
+describe("Verify auth state", () => {
+  test("Displays message from backend if authorized", () => {});
+
+  test("Displays error message if unauthorized", () => {});
+});
