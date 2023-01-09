4e6e473be6c246b76feb84fae320901678344cdf
MaksimSzaszkow <35842893+MaksimSzaszkow@users.noreply.github.com>
2023-01-09 13:59:02 +0100

    PIS-53 useAuth tests

--- src/hooks/useAuth.test.ts
+++ src/hooks/useAuth.test.ts
@@ -1,21 +1,62 @@
-import { useAuth } from "./useAuth";
+import { defaultUser, useAuth } from "./useAuth";
 import { renderHook } from "@testing-library/react";
 import fetch from "jest-fetch-mock";
 import { act } from "react-dom/test-utils";
+import { FirebaseApp } from "firebase/app";
+import { User } from "firebase/auth";
+
+const adminUser = {
+  email: "test@user.com",
+  role: "admin",
+  authorized: true,
+};
+
+const normalUser = {
+  email: "test@user.com",
+  role: "",
+  authorized: true,
+};
+
+jest.mock("firebase/auth", () => ({
+  onAuthStateChanged: (auth: FirebaseApp, fn: (user: User) => Promise<any>) => {
+    observerFn = fn;
+  },
+  getAuth: () => {},
+  signInWithEmailAndPassword: (email: string, password: string) => {},
+  createUserWithEmailAndPassword: (email: string, password: string) => {},
+  signOut: () => {},
+}));
+
+class FirebaseUserMock {
+  email;
+  role;
+
+  constructor(email: string, role: string) {
+    this.email = email;
+    this.role = role;
+  }
+
+  getIdToken() {
+    return "";
+  }
+
+  getIdTokenResult() {
+    return { claims: { role: this.role } };
+  }
+}
+
+let observerFn: Function;
 
 describe("Authorization", () => {
   beforeEach(() => {
     fetch.resetMocks();
   });
-  test("Update user info on login with good credentials", async () => {
+  test("Update user info on successful login", async () => {
     const { result } = renderHook(() => useAuth());
 
-    fetch.mockResponseOnce(
+    fetch.mockOnce(
       JSON.stringify({
-        username: "sherlock",
-        access_token: "token",
-        expires_in: 3600,
-        token_type: "Bearer",
+        token: "",
       })
     );
 
@@ -23,23 +64,49 @@ describe("Authorization", () => {
       async () => await result.current.login("test@user.com", "password123")
     );
 
-    expect(result.current.user).toEqual({
-      username: "sherlock",
-      access_token: "token",
-      expires_in: 3600,
-      token_type: "Bearer",
-    });
-  });
-});
+    expect(result.current.loading).toBe(true);
 
-describe("Logout", () => {
-  test("Clears user data", async () => {
+    await act(async () =>
+      observerFn(new FirebaseUserMock("test@user.com", "admin"))
+    );
+
+    expect(result.current.user).toEqual(adminUser);
+    expect(result.current.loading).toBe(false);
+  });
+
+  test("Update user info on successful register", async () => {
     const { result } = renderHook(() => useAuth());
 
-    act(() => {
-      result.current.logout();
-    });
+    fetch.mockOnce(
+      JSON.stringify({
+        token: "",
+      })
+    );
 
-    expect(result.current.user).toEqual(null);
+    await act(
+      async () => await result.current.register("test@user.com", "password123")
+    );
+
+    expect(result.current.loading).toBe(true);
+
+    await act(async () =>
+      observerFn(new FirebaseUserMock("test@user.com", ""))
+    );
+
+    expect(result.current.user).toEqual(normalUser);
+    expect(result.current.loading).toBe(false);
+  });
+
+  test("Clear user info on successful logout", async () => {
+    const { result } = renderHook(() => useAuth());
+
+    await act(async () => await result.current.logout());
+
+    expect(result.current.loading).toBe(true);
+
+    await act(async () => observerFn(null));
+
+    expect(result.current.user).toEqual(defaultUser);
+    expect(result.current.loading).toBe(false);
   });
 });
